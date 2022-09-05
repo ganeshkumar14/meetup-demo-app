4a8cd028e57db5dcfec9b5912d9c08e3e4030de7
kumargan <ganesh.kumar@hpe.com>
2022-09-05 12:35:02 +0530

    Add backend api to support store and retrive

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,27 @@
+import { useNavigate } from 'react-router-dom';
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+
+    const navigate = useNavigate();
+
+    function addMeetupHandler(meetupData) {
+        fetch('https://meetup-demo-app-245bb-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json',
+        {
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        }).then(() => {
+            navigate("/",{replace: true});
+        })
+    }
+
   return (
     <div>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm addMeetup={addMeetupHandler}/>
     </div>
   );
 }
